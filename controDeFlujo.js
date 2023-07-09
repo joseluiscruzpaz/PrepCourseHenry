33333b1857e316e773dae9b88c084f17d726433e
joseluiscruzpaz <joseluiscruzpaz@yahoo.com>
2023-07-09 13:39:49 -0600

    Ejemplo Control de Flujo

---
+++ controDeFlujo.js
@@ -0,0 +1,10 @@
+function viajar(destino){
+    if (destino === "Brasil") {
+        console.log("Gire a la izquierda");
+    }else if (destino === "Argentina") {
+        console.log("Gire a la Derecha");
+    }else{
+        console.log("Nos Perdimos");
+    }
+}
+viajar("Mex")
