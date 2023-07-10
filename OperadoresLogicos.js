832f26385211043307a55e77cb8022fe06cb3af3
joseluiscruzpaz <joseluiscruzpaz@yahoo.com>
2023-07-09 19:48:25 -0600

    ejemplos de Operadore slogicos

---
+++ OperadoresLogicos.js
@@ -0,0 +1,18 @@
+function mayorYMenor(num){
+    if (num > 5 &&   num < 10 ) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+mayorYMenor(2);
+mayorYMenor(7);
+console.log('------------------')
+function mayorYMenor2(num2) {
+    if (num2 > 5 && num2 < 10 && num2 % 2 === 0) console.log(true);
+    else console.log(false);
+}
+
+mayorYMenor2(7)
+mayorYMenor2(8)
